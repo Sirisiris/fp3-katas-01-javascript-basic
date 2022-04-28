18428d80c00c88e4f90dc1ec33c6ac6eeaebeada
Raul <you@example.com>
2022-04-28 15:55:01 +0200

    first version

--- __tests__/tests.spec.js
+++ __tests__/tests.spec.js
@@ -1,17 +1,22 @@
-/* PRIMERA PARTE: En esta sección deberéis crear las variables necesarias para hacer pasar los tests. 
-Fíjate en el "expect": son las expectativas de nuestro test, es decir, lo que nuestro test busca para poder pasar. 
+/* PRIMERA PARTE: En esta sección deberéis crear las variables necesarias para hacer pasar los tests.
+Fíjate en el "expect": son las expectativas de nuestro test, es decir, lo que nuestro test busca para poder pasar.
 Para lanzar los tests, colócate en la carpeta "__tests__" desde el terminal, y ejecuta el comando "npm test". */
 
 describe("Variables en JavaScript", () => {
     test("Pueden contener strings", () => {
         // Escribe aquí tu código para hacer pasar el "expect" del test
 
+        let a = "Hello!";
+        let b = "Estamos en Femtech";
+
         expect(a).toEqual("Hello!");
         expect(b).toEqual("Estamos en Femtech");
     })
-
     test("Pueden contener números", () => {
         // Escribe aquí tu código
+
+       let c = 2;
+       let d = 1498;
 
         expect(c).toEqual(2);
         expect(d).toEqual(1498);
@@ -20,12 +25,18 @@ describe("Variables en JavaScript", () => {
     test("Pueden contener booleanos", () => {
         // Escribe aquí tu código
 
+       let e = true;
+       let f = false;
+
         expect(e).toEqual(true);
         expect(f).toEqual(false);
     })
 
     test("Pueden contener decimales", () => {
         // Escribe aquí tu código
+
+    let g = 1.45;
+    let h = 45.99999;
 
         expect(g).toEqual(1.45);
         expect(h).toEqual(45.99999);
@@ -39,29 +50,42 @@ describe("Operaciones aritméticas en JavaScript", () => {
     test("Suma", () => {
         // Escribe aquí tu código
 
+        let i = 50;
+        let j = 50;
+
         expect(i+j).toEqual(100);
     })
 
     test("Resta", () => {
         // Escribe aquí tu código
 
+        let k = 20;
+        let l = 10;
         expect(k-l).toEqual(10);
     })
 
     test("Multiplicación", () => {
         // Escribe aquí tu código
 
+        let m = 2;
+        let n = 3;
         expect(m*n).toEqual(6);
     })
 
     test("División", () => {
         // Escribe aquí tu código
 
+        let o = 6;
+        let p = 2;
         expect(o/p).toEqual(3);
     })
 
     test("Módulo: Investiga qué es el operador módulo (%) en JavaScript para resolver este test", () => {
         // Escribe aquí tu código
+
+        let r = 3;
+        let q = 15;
+        let s = 13;
 
         expect(q % r).toEqual(0);
         expect(s % r).toEqual(1);
@@ -76,13 +100,15 @@ describe("Comparaciones en JavaScript", () => {
         const number = 20;
         // Escribe aquí tu código
 
+     let expression = number === 3;
         expect(expression).toEqual(false);
     })
 
     test("Usando el comparador de diferencia !==", () => {
         const language = "JavaScript";
         // Escribe aquí tu código
-        
+
+        let expression = language !== "JavaScript";
         expect(expression).toEqual(false);
     })
 })
@@ -91,8 +117,11 @@ describe("Álgebra booleana", () => {
     test("Usando el operador AND (&&)", () => {
         // Escribe aquí tu código
 
+
+        let a = true;
+        let b = false;
         const expression = a && b;
-    
+
         expect(expression).toEqual(true);
     })
 
